35ad44ebecd84db86ff0da21708bef8896472d7d
27tr <114559221+MCDream3@users.noreply.github.com>
2023-03-15 06:53:43 +0000

    dodano kod ONP

--- ONP.py
+++ ONP.py
@@ -1,4 +1,18 @@
 #ONP --> Odwrotne Notowanie Polskie
+
+s = input()
+stos = []
+for elem in s:
+  if elem.isdigit() : stos.append(int(elem))
+  else:
+    b, a = stos.pop(), stos.pop()
+    if elem == "+" : stos.append(a+b)
+    elif elem == "*" : stos.append(a*b)
+    elif elem == "-" : stos.append(a-b)
+    elif elem == "/" : stos.append(a//b)
+  print(stos)
+
+
 #FIFO --> First In First Out
 #LIFO --> Last In First Out
 
